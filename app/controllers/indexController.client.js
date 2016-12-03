bfe097c66ba835ffffa765cc84bfb0fcfc8852ee
Jestoni Salonga <jestonisalonga@gmail.com>
2016-12-03 16:28:58 +0000

    Added angular framework

---
+++ app/controllers/indexController.client.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function () {
+   angular
+      .module('VotingApp', ['ngResource'])
+      .controller('indexController', ['$scope', '$resource', function ($scope, $resource) {
+         var User = $resource('/api/user');
+
+         $scope.getUser = function () {
+            User.get(function (result) {
+               if (result.displayName !== null) {
+                  $("#authorized-navbar").removeClass("hide");
+                  $("#unauthorized-navbar").addClass("hide");
+               }
+            });
+         };
+
+         $scope.getUser();
+      }]);
+})();
---
+++ app/controllers/mypollsController.client.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function () {
+   angular
+      .module('VotingApp', ['ngResource'])
+      .controller('mypollsController', ['$scope', '$resource', function ($scope, $resource) {
+         var User = $resource('/api/user');
+
+         $scope.getUser = function () {
+            User.get(function (result) {
+               if (result.displayName !== null) {
+                  $("#authorized-navbar").removeClass("hide");
+                  $("#unauthorized-navbar").addClass("hide");
+               }
+            });
+         };
+
+         $scope.getUser();
+      }]);
+})();
