db4a111971672cbb03114382a0860b4943467f70
Jestoni Salonga <jestonisalonga@gmail.com>
2016-12-06 20:31:57 +0000

    Added facebook sharing

--- app/controllers/publicpollController.client.js
+++ app/controllers/publicpollController.client.js
@@ -1,5 +1,22 @@
 'use strict';
 
+window.fbAsyncInit = function() {
+   FB.init({
+      appId      : '1149618391821610',
+      xfbml      : true,
+      version    : 'v2.8'
+   });
+   FB.AppEvents.logPageView();
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 (function () {
    angular
       .module('VotingApp', ['ngResource'])
@@ -131,7 +148,11 @@
          
          /***** USER CONTROLS *****/
          $scope.sharePoll = function () {
-            
+            FB.ui({
+               method: 'share',
+               display: 'popup',
+               href: window.location.href,
+            }, function(response){});
          };
          
          $scope.submitVote = function () {
